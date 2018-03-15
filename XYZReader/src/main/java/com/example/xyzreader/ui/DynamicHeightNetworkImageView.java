18dfa6477301ba3e5b42ff39e48258b6e9b8093a
Daniel Tsvetilov <daniel.tsvetilov@gmail.com>
2018-03-15 15:00:28 +0200

    initial

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
@@ -0,0 +1,29 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+public class DynamicHeightNetworkImageView extends NetworkImageView {
+
+    public DynamicHeightNetworkImageView(Context context) {
+        super(context);
+    }
+
+    public DynamicHeightNetworkImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DynamicHeightNetworkImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int threeTwoHeight = MeasureSpec.getSize(widthMeasureSpec) * 2 / 3;
+        int threeTwoHeightSpec = MeasureSpec.makeMeasureSpec(threeTwoHeight, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, threeTwoHeightSpec);
+    }
+}
